581eb74200f6e1603465da9f71f033bf8388dd54
Daniel <dhonaruto@gmail.com>
2021-01-22 21:50:53 -0500

    overwritten by checkout

---
+++ routes/campusDelete.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+//Get model
+const model = require('../database/models');
+
+//Get a campus
+router.get('/:id', function(req, res, next){ 
+    model.Campus.findByPk(req.params.id)
+    .then(campus => {
+        if (!campus){
+            res.status(404)
+            .json({
+                message: "CANNOT FIND CAMPUS"
+            })
+        }
+
+        campus.destroy();
+
+        res.status(200)
+        .json({
+            message: "CAMPUS IS DELETED"
+        });
+    })
+    .catch(error => {
+        res.status(404)
+        .json({
+            message: "ERROR",
+            error
+        });
+    })
+});
+
+// Export our router, so that it can be imported to construct our apiRouter;
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,28 @@ const router = express.Router();
 const studentAllRouter = require('./studentAll');
 const studentOneRouter = require('./studentOne');
 const studentCreateRouter = require('./studentCreate');
-//const studentDeleteRouter = require('./studentDelete');
+const studentDeleteRouter = require('./studentDelete');
+const studentUpdateRouter = require('./studentUpdate');
 
 const campusAllRouter = require('./campusAll');
 const campusOneRouter = require('./campusOne');
 const campusCreateRouter = require('./campusCreate');
-//const campusDeleteRouter = require('./campusDelete');
+const campusDeleteRouter = require('./campusDelete');
+const campusUpdateRouter = require('./campusUpdate');
 
 
 // Mount our subrouters to assemble our apiRouter;
 router.use('/studentAll', studentAllRouter);
 router.use('/studentOne', studentOneRouter);
 router.use('/studentCreate', studentCreateRouter);
-//router.use('/studentDelete', studentDeleteRouter);
+router.use('/studentDelete', studentDeleteRouter);
+router.use('/studentUpdate', studentUpdateRouter);
 
 router.use('/campusAll', campusAllRouter);
 router.use('/campusOne', campusOneRouter);
 router.use('/campusCreate', campusCreateRouter);
-//router.use('/campusDelete', campusDeleteRouter);
+router.use('/campusDelete', campusDeleteRouter);
+router.use('/campusUpdate', campusUpdateRouter);
 
 
 // Error handling middleware;
