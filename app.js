319a7603d7ced4a2192ebc29608951655337972d
Dennis Orsini <dennisorsini97@gmail.com>
2021-01-25 04:52:30 -0500

    8080 fallback port, dev script

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use('/routes', require('./routes'));
 
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Listening to port ${process.env.PORT || 8080}`);
+})
 
 
 // // Here, we will sync our database, create our application, and export this module so that we can use it in the bin directory, where we will be able to establish a server to listen and handle requests and responses;
